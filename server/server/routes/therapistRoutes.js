866cace84a1e680132e8787e222b0e75262bff83
Raja Abdullah <72712630+Rjprograms1999@users.noreply.github.com>
2025-08-02 01:05:14 +0500

    Delete server/server directory

--- server/server/routes/therapistRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { protect, requireRole } from "../controllers/authController.js";
-import { getAllUsers } from "../controllers/therapistController.js";
-
-const router = express.Router();
-
-router.get("/users", protect, requireRole("therapist"), getAllUsers);
-
-export default router;
